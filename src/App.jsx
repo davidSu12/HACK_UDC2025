d30a80269cef476ac4c2985fce58da3591d2f2bc
borja.garcia.barroso <borja.garcia.barroso@udc.es>
2025-02-22 07:58:10 +0100

    React con rutas

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,17 @@ import { Chat } from "./pages/Chat";
 import { Error } from "./pages/Error";
 import { HomeStudent } from "./pages/Student/HomeStudent";
 import { HomeTeacher } from "./pages/Teacher/HomeTeacher";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 export function App() {
-  return <Error numberError="404" />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomeStudent />} />
+        <Route path="/contact" element={<BlogEntries />} />
+        <Route path="*" element={<Error />} />{" "}
+        {/* Página por defecto si no encuentra la ruta */}
+      </Routes>
+    </Router>
+  );
 }
